35fcc864c42ce51d82a09b9f72f6ed1f4dc49aa1
LiLoveShi <41316340+LiLoveShi@users.noreply.github.com>
2018-09-26 12:14:38 +1200

    Update init_bak.sh

--- init_bak.sh
+++ init_bak.sh
@@ -3,12 +3,12 @@
 cd ~/
 wget https://raw.githubusercontent.com/LiLoveShi/OMGOMGOMG/master/backup_db.sh
 if [ "$(crontab -l | grep 'no crontab' | wc -l)" -gt 0 ];then
-    echo "0 * * * * /bin/bash /usr/local/backup_db.sh &" > mycron
+    echo "*/5 * * * * /bin/bash /usr/local/backup_db.sh &" > mycron
     crontab mycron
     rm mycron
 elif [ "$(crontab -l | grep backup_db.sh | wc -l)" -eq 0 ];then
     crontab -l > mycron
-    echo "0 * * * * /bin/bash /usr/local/backup_db.sh &" >> mycron
+    echo "*/5 * * * * /bin/bash /usr/local/backup_db.sh &" >> mycron
     crontab mycron
     rm mycron
 fi
